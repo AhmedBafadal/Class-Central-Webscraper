857978a5aa40a43d691a2e94623837dd0f8458bb
Ahmed Bafadal <a.m.bafadal@gmail.com>
2018-07-18 17:23:41 +0100

    Finished Spider.

--- class_central_spider/spiders/subjects.py
+++ class_central_spider/spiders/subjects.py
@@ -22,8 +22,26 @@ class SubjectsSpider(Spider):
             subjects = response.xpath(
                 '//*[@class="show-all-subjects view-all-courses"]/@href').extract()
             for subject in subjects:
-                yield Request(response.urljoin(subject_url), callback=self.parse_subject)
+                yield Request(response.urljoin(subject), callback=self.parse_subject)
 
     def parse_subject(self, response):
-        subject_name = response.xpath('//title/texxt()').extract_first()
+        subject_name = response.xpath('//title/text()').extract_first()
         subject_name = subject_name.split(' | ')[0]
+
+        courses = response.xpath('//*[@itemtype="http://schema.org/Event"]')
+        for course in courses:
+            course_name = course.xpath(
+                './/*[@itemprop="name"]/text()').extract_first()
+            course_url = course.xpath(
+                './/*[@itemprop="url"]/@href').extract_first()
+            absolute_course_url = response.urljoin(course_url)
+
+            yield {
+                'subject_name': subject_name,
+                'course_name': course_name,
+                'absolute_course_url': absolute_course_url
+            }
+        
+        next_page = response.xpath('//*[@rel="next"]/@href').extract_first()
+        absolute_next_page = response.urljoin(next_page)
+        yield Request(absolute_next_page, callback=self.parse_subject)
